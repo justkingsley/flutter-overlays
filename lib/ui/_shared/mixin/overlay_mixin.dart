3eae6e7831d0cd3c8f4d90149f5f03d0ea27cbb6
Just_Kingsley <kingsleybingala@gmail.com>
2022-09-13 13:39:47 +0200

    Finished Overlay Mixin

--- lib/ui/_shared/mixin/overlay_mixin.dart
+++ lib/ui/_shared/mixin/overlay_mixin.dart
@@ -7,29 +7,35 @@ mixin OverlayStateMixin<T extends StatefulWidget> on State<T>{
 
   OverlayEntry? _overlayEntry;
 
-  void removeOverlay(){
+  void removeOverlay() {
     _overlayEntry?.remove();
     _overlayEntry = null;
   }
 
-  Widget _dismissibleOverlay(Widget child) => Stack(
-    children: [
-      Positioned.fill(child: ColoredBox(
-        color: AppColors.barrierColor,
-        child: GestureDetector(
-          onTap: removeOverlay,
+  Widget _dismissibleOverlay(Widget child) =>
+      Stack(
+        children: [
+          Positioned.fill(child: ColoredBox(
+            color: AppColors.barrierColor,
+            child: GestureDetector(
+              onTap: removeOverlay,
+            ),
           ),
-        ),
-      ),
-      child,
-    ],
-  );
+          ),
+          child,
+        ],
+      );
 
-  void _insertOverlayWidget(Widget child){
+  void _insertOverlay(Widget child) {
     _overlayEntry = OverlayEntry(
         builder: (_) => _dismissibleOverlay(child)
     );
 
+    Overlay.of(context)?.insert(_overlayEntry!);
   }
+
+  bool get isOverlayShown => _overlayEntry != null;
+  void toggleOverlay(Widget child) =>
+      isOverlayShown ? removeOverlay() : _insertOverlay(child);
 
 }
